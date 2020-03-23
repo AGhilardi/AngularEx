3f8b873bd0c0ca54b551a3ef9070a49c443e763d
AGhilardi <57990391+AGhilardi@users.noreply.github.com>
2020-03-23 10:06:51 +0100

    creazione file e models

--- AngularEX/src/app/app.module.ts
+++ AngularEX/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponentComponent } from './components/menu-component/menu-component.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { GamesListComponent } from './components/games-list/games-list.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
+import { EditGameComponent } from './components/edit-game/edit-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponentComponent,
+    MenuComponent,
+    HomeComponent,
+    GamesListComponent,
+    GameDetailComponent,
+    EditGameComponent
   ],
   imports: [
     BrowserModule
---
+++ AngularEX/src/app/model/game-item.ts
@@ -0,0 +1,9 @@
+export interface GameItem {
+    id:string;
+    nome:string;
+    desc:string;
+    gen:string;
+    rate:number;
+    price:number;
+    year:number;
+}
